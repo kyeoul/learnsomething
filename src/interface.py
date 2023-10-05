9b65b5ec5310a3f7455ea30d2298797aa00eebf1
kyeoul <54390304+kyeoul@users.noreply.github.com>
2023-10-05 17:49:18 -0500

    Preliminary implementations

--- src/interface.py
+++ src/interface.py
@@ -1,13 +1,22 @@
 class Exchange:
     # implement this!
 
+    balance = 0
+
     def __init__(self, initialBalance):
         """Initial Balance is the amount that each account should start with."""
-        pass
+        self.balance = initialBalance
 
+    # Let's suppose that trade has three parameters buy/sell, quantity, and price
     def add_trade(self, trade):
         """Adds a trade to the exchange (validation required)
         and returns a match if required. It is up to you on how you will
         handle representing trades. """
+
+        if trade.buy:
+            balance -= trade.quantity * trade.price
+        else:
+            balance += trade.quantity * trade.price
+
         raise NotImplementedError
 
